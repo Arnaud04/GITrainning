84e69a28e1732d9975df722baa927e987ce6cc8c
Arnaud <arnaudconver7@gmail.com>
2021-05-14 14:54:29 +0200

    Main function and sum function.

---
+++ myprogram.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char * argv[])
+{
+  printf("Debut du main\n");
+
+  return 0;
+}
+
+/*
+** Sum elements 
+*/
+int sum(int a, int b)
+{
+  return a+b;
+}
